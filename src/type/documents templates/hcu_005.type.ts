6da4773fd8b58c919be54a51ff48aa09ebf71a65
khiabetlasso <khiabet_lasso@hotmail.com>
2025-08-07 08:21:44 -0500

    modified:   src/generation/templates/hcu_005.template.hbs 	modified:   src/generation/templates/hcu_005.template.hbs.json 	modified:   src/type/documents templates/hcu_005.type.ts

--- src/type/documents templates/hcu_005.type.ts
+++ src/type/documents templates/hcu_005.type.ts
@@ -1,1 +1,36 @@
-export type Hcu005DocumentTemplate = {};
+export type Hcu005DocumentTemplate = {
+    //DATOS DEL ESTABLECIMIENTO Y USUARIO/PACIENTE
+    A: {
+      institucion_sistema: string;
+        unicodigo: number;
+        establecimiento_salud: string;
+        numero_historia_clinica: string;
+        numero_archivo: string;
+        numero_hoja: string;
+        primer_apellido: string;
+        segundo_apellido: string;
+        primer_nombre: string;
+        segundo_nombre: string;
+        sexo: string;
+        edad: string;
+        condicion_edad: {
+            h: boolean;
+            d: boolean;
+            m: boolean;
+            a: boolean;
+        };
+    }, 
+    
+    //EVOLUCION Y PRESCRIPCIONES
+    B: {
+      evolucion:{
+        fecha: string;
+        hora: string;
+        nota_evolucion: string;
+      },
+        prescripciones: {
+            farmaco_indicacion: string;
+            administracion_farmacos: string;
+        };
+    },
+};
